68ebee7cd47f66922437e1f7e842bf876483aaff
Alailson Nascimento <alailsonnascimento707@gmail.com>
2026-07-01 21:08:21 -0300

    Adiciona exercicio 03 - numeros pares com loop for

---
+++ 01-logica-de-programacao/exercicio-03.js
@@ -0,0 +1,12 @@
+// Declara a variavel
+let valor = 123;
+
+// Criando um loop for
+
+for (let i = 1; i <= valor; i++) {
+
+    // verifica se o resto da divisão por 2 for 0, é par
+    if (i % 2 === 0) {
+        console.log(i); // mostra no console resultado com o número par
+    }
+}
